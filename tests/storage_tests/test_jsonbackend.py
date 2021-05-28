bc76e55c7e8d5e121f6e61219d21c44c79a7ac38
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-05-28 11:21:03 +0200

    first take on the jsonbackend storage tests

---
+++ tests/storage_tests/test_jsonbackend.py
@@ -0,0 +1,29 @@
+import unittest
+
+from apiserver.storage.JsonFileStorageAdapter import JsonFileStorageAdapter
+from apiserver.storage import LocationDataType
+from collections import namedtuple 
+import os
+import pathlib
+import shutil
+
+
+class SomeTests(unittest.TestCase):
+    def setUp(self):
+
+        Settings =  namedtuple('Settings',['json_storage_path'])
+        self.test_config = Settings('/tmp/json_test/')
+        pathlib.Path(self.test_config.json_storage_path).mkdir(parents=True, exist_ok=True)
+
+        self.store = JsonFileStorageAdapter(self.test_config)
+    
+    def tearDown(self):
+        if os.path.exists(self.test_config.json_storage_path):
+            print('Paht exists. Removing')
+            shutil.rmtree(self.test_config.json_storage_path)
+
+    def test_getList(self):
+        test_type = LocationDataType.DATASET
+        lst = self.store.getList(type=test_type)
+        self.assertEqual(lst, {}, 'Id should not be none')
+        
