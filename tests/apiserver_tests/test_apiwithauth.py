c2d0ef5244a090bc5607c29cc5b2157968f6f831
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-06-07 11:59:56 +0200

    pytest compatible testing

--- tests/apiserver_tests/test_apiwithauth.py
+++ tests/apiserver_tests/test_apiwithauth.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
-from apiserver.main import app, my_user, my_auth
+from context import apiserver, storage
+#from apiserver import app, my_user, my_auth
+
 from unittest import TestCase
 from apiserver.security.user import User
 
@@ -9,26 +11,25 @@ def myfunc():
 class UserTests(TestCase):
 
     def setUp(self):
-        self.client = TestClient(app)
-        app.dependency_overrides={}
-
+        apiserver.app.dependency_overrides[apiserver.main.my_auth] = myfunc
+        apiserver.app.dependency_overrides[apiserver.main.my_user] = myfunc
+        
+        self.client = TestClient(apiserver.app)
+        
     def tearDown(self):
-        app.dependency_overrides={}
+        apiserver.app.dependency_overrides={}
     
     def test_me(self):
-       app.dependency_overrides[my_user] = myfunc
        
        resp = self.client.get('/me')
-       self.assertEquals(resp.json(), {'username': 'foo', 'email': 'bar'})
+       self.assertEqual(resp.json(), {'username': 'foo', 'email': 'bar'})
 
     def test_token(self):
-        app.dependency_overrides[my_auth] = myfunc
         rsp = self.client.post('/token').json()
         self.assertEqual(rsp["token_type"], "bearer")
         self.assertTrue('access_token' in rsp)
 
     def test_create(self):
-        app.dependency_overrides[my_user] = myfunc
         my_data = {
             'name': 'some dataset', 
             'url': 'http://loc.me/1', 
