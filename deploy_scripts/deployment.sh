ad57e494bfdc406b368fca19a3a06e723f081134
Christian Boettcher <c.boettcher@fz-juelich.de>
2021-08-05 09:20:55 +0200

    set domain for certificate in CI/CD and deploy scripts

--- deploy_scripts/deployment.sh
+++ deploy_scripts/deployment.sh
@@ -8,10 +8,13 @@ git pull --all
 if [ -z "$CHECKOUT_TAG" ]; then export CHECKOUT_TAG=master; fi
 git checkout $CHECKOUT_TAG
 if [ -z "$API_URL" ]; then export API_URL=https://datacatalog.fz-juelich.de/; fi
+if [ -z "$SERVER_DOMAIN" ]; then export SERVER_DOMAIN=datacatalog.fz-juelich.de; fi
 
 pip install -r requirements.txt
 
 python3 frontend/createStatic.py -u $API_URL
+
+sed -i "s_datacatalog.fz-juelich.de_${SERVER_DOMAIN}_g" docker-compose.yml
 
 # it is at this point assumed that ip and volume are correctly assigned, and that dns is working properly
 
