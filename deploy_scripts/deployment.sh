6069fb75dd2502da09c9bb105e4c2530d47ab80b
Christian Boettcher <c.boettcher@fz-juelich.de>
2021-08-05 08:50:48 +0200

    create needed docker network, run everything with full paths, install reqs, fix mount dir for testing server

--- deploy_scripts/deployment.sh
+++ deploy_scripts/deployment.sh
@@ -8,6 +8,9 @@ git pull --all
 if [ -z "$CHECKOUT_TAG" ]; then export CHECKOUT_TAG=master; fi
 git checkout $CHECKOUT_TAG
 if [ -z "$API_URL" ]; then export API_URL=https://datacatalog.fz-juelich.de/; fi
+
+pip install -r requirements.txt
+
 python3 frontend/createStatic.py -u $API_URL
 
 # it is at this point assumed that ip and volume are correctly assigned, and that dns is working properly
