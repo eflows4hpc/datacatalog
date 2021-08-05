d996335affce6819138a89a6782427dc32be3e1f
Christian Boettcher <c.boettcher@fz-juelich.de>
2021-08-05 07:40:11 +0200

    initial deployment scripts, use master tag instead of latest for test deployment

---
+++ deploy_scripts/deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OLD_DIR=`pwd`
+
+cd $1
+
+git pull --all
+if [ -z "$CHECKOUT_TAG" ]; then export CHECKOUT_TAG=master; fi
+git checkout $CHECKOUT_TAG
+if [ -z "$API_URL" ]; then export API_URL=https://datacatalog.fz-juelich.de/; fi
+python3 frontend/createStatic.py -u $API_URL
+
+# it is at this point assumed that ip and volume are correctly assigned, and that dns is working properly
+
+nohup docker-compose up >/app/mnt/docker.log & # or similar to capture docker log TODO
+
+cd $OLD_DIR
